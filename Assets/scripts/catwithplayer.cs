fc37bd82374e6e61b802271f945eb64280224546
ZoidbergForPresident <zoidbergforpresident@outlook.com>
2016-10-15 17:38:13 +0200

    Clean-up commit

--- Assets/scripts/catwithplayer.cs
+++ Assets/scripts/catwithplayer.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class catwithplayer : MonoBehaviour {
+public class catwithplayer : MonoBehaviour
+{
 
     [SerializeField]
     private float force = 1;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Jeanmi");
-        if (other.gameObject.tag == "Player") {
+        if (other.gameObject.tag == "Player")
+        {
             Debug.Log("Robert");
             //Vector3 rowX = new Vector3(0.00000001f, 0.0000000001f, 0);
             //other.gameObject.GetComponent<Rigidbody>().AddForce(rowX);
             //other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * -1);
-            VacuumBotController.control = false;
+            //VacuumBotController.control = false;
+            other.gameObject.GetComponent<VacuumBotController>().HasControl = false;
             // Calculate Angle Between the collision point and the player
-            if(other.GetComponent<VacuumBotController>().IsGrounded)
+            if (other.GetComponent<VacuumBotController>().IsGrounded)
             {
                 other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 0.5f, other.transform.position.z);
             }
@@ -36,18 +41,24 @@ public class catwithplayer : MonoBehaviour {
             // And finally we add force in the direction of dir and multiply it by force. 
             // This will push back the player
             other.GetComponent<Rigidbody>().AddForce(dir * force);
-            Debug.Log(VacuumBotController.control);
-/*            if (other.gameObject.tag == "Floor")
-            {
-                VacuumBotController.control = true;
-            }*/
+            //Debug.Log(VacuumBotController.HasControl);
+            Debug.Log("Player has control: " + other.gameObject.GetComponent<VacuumBotController>().HasControl);
+            /*            if (other.gameObject.tag == "Floor")
+                        {
+                            VacuumBotController.control = true;
+                        }*/
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        VacuumBotController.control = true;
-        Debug.Log(VacuumBotController.control);
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<VacuumBotController>().HasControl = true;
+        }
+        //VacuumBotController.control = true;
+        //Debug.Log(VacuumBotController.control);
+        Debug.Log("Player has control: " + other.gameObject.GetComponent<VacuumBotController>().HasControl);
     }
 
 }
