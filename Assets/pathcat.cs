3a8b0a26073b3844578ec5638316e192ac965a0a
naloun <naloun@hotmail.be>
2016-10-15 02:09:04 +0200

    Init

---
+++ Assets/pathcat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class pathcat : MonoBehaviour {
+
+    public List<Transform> target;
+	private int random = 3;
+	private int random2 = 1;
+
+    [SerializeField]
+    private float distance = 2f;
+    
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+        VariablePosition();
+	}
+
+	void VariablePosition () {
+		if (Vector3.Distance (transform.position, target[random].position) > distance) {
+            agent.destination = target[random].position;
+		}
+		else{
+            random2 = Random.Range(0, target.Count);
+            if (random == random2 || (random == 0 && random2 == 0)) {
+                random2++;
+                if(random2 >= target.Count){
+                    random2 = 1;
+                }
+            }
+			random = random2;            
+        }
+    }
+
+}
